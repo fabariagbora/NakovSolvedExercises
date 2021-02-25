afd09e59e3d7f6a96a5ac040a557420773fd4fbd
fabariagbora <79636015+fabariagbora@users.noreply.github.com>
2021-02-25 11:32:20 +0100

    initial commit if nakov

---
+++ Chapter_1/Solution_1/SolutionOne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NakovSolvedExercises.Chapter_1.Solution_1
+{
+    class SolutionOne
+    {
+        public void DisplaySomething()
+        {
+            Console.WriteLine("testing class one");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NakovSolvedExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutionOne = new NakovSolvedExercises.Chapter_1.Solution_1.SolutionOne();
+
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine(solutionOne);
+            Console.ReadLine();
+        }
+    }
+}
